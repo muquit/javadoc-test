f6eca9519d2581c4e48af937603eb325d312a001
muquit <muquit@gmail.com>
2016-10-23 15:14:53 -0400

    .

--- gen_javadoc.sh
+++ gen_javadoc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # muquit@muquit.com Oct-23-2016 
 #javadoc -d ~/x -stylesheetfile ~/Downloads/java/javadoc_css/stylesheet.css  $(find . -name *.java)
-/bin/rm -f ./docs/*
+/bin/rm -rf ./docs/*
 javadoc -d ./docs  $(find . -name *.java)
 
